224b68b6125f553c3bb4a4743eeaa4348725e0bd
Ange230700 <kouakouangeericstephane@gmail.com>
2023-10-23 22:32:07 +0200

    routing

--- nodejs-http/server.js
+++ nodejs-http/server.js
@@ -2,7 +2,16 @@ const http = require('http');
 const port = 8000;
 const requestHandler = (request, response) => {
     console.log(request.url);
-    response.end('Hello Node.js Server!');
+    // response.end('Hello Node.js Server!');
+    if (request.url === '/') {
+        response.end('Hello Node.js Server!');
+    }
+    else if (request.url === '/about') {
+        response.end('This demonstrates routing in Node.js!');
+    }
+    else {
+        response.end('Default page (URLs other than / and /about)')
+    }
 }
 const server = http.createServer(requestHandler);
 server.listen(
