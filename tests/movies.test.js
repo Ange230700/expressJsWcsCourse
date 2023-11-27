dbfa5e32648414865293ce72ab201f8d2d00a09f
Ange230700 <kouakouangeericstephane@gmail.com>
2023-11-27 11:17:54 +0100

    getting done with the challenge part 2

--- tests/movies.test.js
+++ tests/movies.test.js
@@ -19,10 +19,21 @@ describe(
   'GET /api/movies/:id',
   () => {
     it('should return a movie with the given id', async () => {
-      const response = await request(app).get('/api/movies/4');
+      const response = await request(app).get('/api/movies/1');
 
       expect(response.headers['content-type']).toMatch(/json/);
       expect(response.status).toEqual(200);
+    });
+  },
+);
+
+describe(
+  'GET /api/movies/:id',
+  () => {
+    it('should return a 404 status for inexistant id', async () => {
+      const response = await request(app).get('/api/movies/0');
+
+      expect(response.status).toEqual(404);
     });
   },
 );
